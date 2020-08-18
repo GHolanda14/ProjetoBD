5cdd8fdcc410c592445517f5b0a9303e1407d312
GHolanda14 <42251349+GHolanda14@users.noreply.github.com>
2020-08-18 08:39:13 -0300

    Updating

--- Sistema/src/pojo/Evento.java
+++ Sistema/src/pojo/Evento.java
@@ -95,15 +95,7 @@ public class Evento {
         PartEventoDAO ld = new PartEventoDAO();
         return "["+nome_evento+"]\nLocal: "+ld.localName(local_evento)+"\nData: "+dat[2]+"/"+dat[1]+"/"+dat[0]+"\nHorario: "+hora_evento.toString();
     }
-    public Evento(String nome,String descricao,LocalDate data,LocalTime hora,int local,int qtd){
-        nome_evento = nome;
-        descricao_evento = descricao;
-        local_evento = local;
-        data_evento = data;
-        hora_evento = hora;
-        qtd_participante = qtd;        
-    }
-    public Evento(int id,String nome,String descricao,LocalDate data,LocalTime hora,int local,int qtd){
+    public Evento(String nome,String descricao,LocalDate data,LocalTime hora,int local,int qtd, int... id){
         nome_evento = nome;
         descricao_evento = descricao;
         local_evento = local;
